35989f70559dcc6072094c5a83a74a8ef34842b2
S I M O N. P <spau@hotmail.de>
2022-03-05 13:02:02 +0100

    Added Server Interface

--- common/networking/NetConnection.h
+++ common/networking/NetConnection.h
@@ -18,7 +18,7 @@ namespace sim
             };
 
         public:
-            Connection(owner parent, asio::io_context context, asio::ip::tcp::socket socket, uint16_t port, TSQueue<OwnedMessage<T>> &qMessagesIn) // owner = server | client, context = needs an asio context to run in, takes over the socket and handles connection, port = port at which to connect, Queue of Messages comming in
+            Connection(owner parent, asio::io_context context, asio::ip::tcp::socket socket, TSQueue<OwnedMessage<T>> &qMessagesIn) // owner = server | client, context = needs an asio context to run in, takes over the socket and handles connection, port = port at which to connect, Queue of Messages comming in
                 : m_context(context);
             m_socket(socket);
             m_qMessagesIn(m_qMessagesIn)
@@ -30,7 +30,7 @@ namespace sim
             }
 
         public:
-            bool connect_to_server(const asio::ip::tcp::resolver::results_type &endpoint)
+            bool connect_to_server(const asio::ip::tcp::resolver::results_type &endpoint) // function only used by clients to connect to the server provided via an endpoint
             {
                 if (m_nowner == owner::client)
                 {
@@ -39,20 +39,20 @@ namespace sim
                                         {
                                             if (!ec)
                                             {
-                                                mf_read_header();
+                                                mf_read_header(); // prime the asio context to start watching for messages
                                             }
                                         });
                 }
             }
 
-            bool connect_to_client(uint32_t uid = 0)
+            bool connect_to_client(uint32_t uid = 0) // function only used by the server to connect to a client giving him the provided uid
             {
                 if (m_nowner == owner::server)
                 {
-                    if (m_socket.is_open())
+                    if (m_socket.is_open()) // check if the connection to client has already been established -> done by client
                     {
-                        m_id = uid;
-                        mf_read_header();
+                        m_id = uid;       // this connection now has a unique id
+                        mf_read_header(); // prime the asio context to start watching for headers
                     }
                 }
             }
@@ -77,13 +77,13 @@ namespace sim
                 return m_id;
             }
 
-            bool send(const Message<T> &msg)
+            bool send(const Message<T> &msg) // primes the asio context with a new task of writing the provided message
             {
                 asio::post(m_context,
                            [this, msg]()
                            {
-                               bool b_is_already_writing_messages = !m_qMessagesOut.empty();
-                               m_qMessagesOut.push_back(msg);
+                               bool b_is_already_writing_messages = !m_qMessagesOut.empty(); // if this flag is set it means the asio context is already writing all the messages contained in m_qMessagesOut
+                               m_qMessagesOut.push_back(msg);                                // add to outgoing queue
                                if (!b_is_already_writing_messages)
                                {
                                    mf_write_header();
@@ -92,7 +92,7 @@ namespace sim
             }
 
         private:
-            void mf_read_header()
+            void mf_read_header() // automaticaly calls read body function or calls itself again if there is no body to read
             {
                 asio::async_read(m_socket, asio::buffer(m_tempMessageIn.header, sizeof(MessageHeader<T>)),
                                  [this](std::error_code ec, std::size_t length)
@@ -101,12 +101,12 @@ namespace sim
                                      {
                                          if (m_tempMessagesIn.header.size > 0)
                                          {
-                                             m_tempMessagesIn.body.resize(m_tempMessagesIn.header.size);
-                                             mf_read_body();
+                                             m_tempMessagesIn.body.resize(m_tempMessagesIn.header.size); // allocate the space for the body
+                                             mf_read_body();                                             // prime context to read the body
                                          }
                                          else
                                          {
-                                             mf_add_to_incomming_message_queue();
+                                             mf_add_to_incomming_message_queue(); // message has no body so just add it to the queue already -> calls mf_read_header() again
                                          }
                                      }
                                      else
@@ -117,14 +117,14 @@ namespace sim
                                  });
             }
 
-            void mf_read_body()
+            void mf_read_body() // reads the body of the messages already stored in tempMessage -> only called by mf_read_header()
             {
                 asio::async_read(m_socket, asio::buffer(m_tempMessageIn.body, m_tempMessageIn.body.size()),
                                  [this](std::error_code ec, size_t lenght)
                                  {
                                      if (!ec)
                                      {
-                                         mf_add_to_incomming_message_queue();
+                                         mf_add_to_incomming_message_queue(); // add message to m_qMessagesIn
                                      }
                                      else
                                      {
@@ -134,37 +134,37 @@ namespace sim
                                  });
             }
 
-            void mf_add_to_incomming_message_queue()
+            void mf_add_to_incomming_message_queue() // adds message in m_tempMessageIn to incomming message stream
             {
-                if (m_nowner == owner::server)
+                if (m_nowner == owner::server) // if the server owns the connection object make him own the message
                 {
-                    m_qMessagesIn.push_back({this->shared_from_this(), m_tempMessageIn});
+                    m_qMessagesIn.push_back({this->shared_from_this(), m_tempMessageIn}); // push back new Owned_Message primed to the server
                 }
                 else
                 {
                     m_qMessagesIn.push_back({nullptr, m_tempMessageIn});
                 }
 
-                mf_read_header();
+                mf_read_header(); // prime asio context with further reading work
             }
 
-            void mf_write_header()
+            void mf_write_header() // writes the first header in outgoing message queue to the socket and calls mf_write_body() function -> starts ones the first message is pushed into the queue by send()
             {
                 asio::async_write(m_socket, asio::buffer(&m_qMessagesOut.front().header, sizeof(MessageHeader<T>)),
                                   [this](std::error_code ec, size_t length)
                                   {
                                       if (!ec)
                                       {
-                                          if (m_qMessagesOut.front().body.size() > 0)
+                                          if (m_qMessagesOut.front().body.size() > 0) // see if we have to write a body
                                           {
-                                              mf_write_body();
+                                              mf_write_body(); // calls mf_write_header() again if there are more messages so the context has a workload
                                           }
                                           else
                                           {
-                                              m_qMessagesOut.pop_front();
+                                              m_qMessagesOut.pop_front(); // means only consisted of a header so remove the message from the queue
                                               if (!m_qMessagesOut.empty())
                                               {
-                                                  mf_write_header();
+                                                  mf_write_header(); // write until queue is empty
                                               }
                                           }
                                       }
@@ -176,14 +176,14 @@ namespace sim
                                   });
             }
 
-            void mf_write_body()
+            void mf_write_body() // writes the body of the front message in the outgoing queue to the socket -> called by mf_write_header() -> calls mf_write_header again if there are still messages left
             {
                 asio::async_write(m_socket, asio::buffer(m_qMessagesOut.front().body.data(), m_qMessagesOut.front().body.size()),
                                   [this](std::error_code ec, size_t length)
                                   {
                                       if (!ec)
                                       {
-                                          m_qMessagesOut.pop_front();
+                                          m_qMessagesOut.pop_front(); // message has successfully been transmitted we can remove the message from the queue and work on the next one
                                           if (!m_qMessagesOut.empty())
                                           {
                                               mf_write_header();
--- common/networking/NetServerInterface.h
+++ common/networking/NetServerInterface.h
@@ -11,10 +11,144 @@ namespace sim
         template <typename T>
         class Server_Interface
         {
+        public:
+            Server_Interface(uint16_t port) : m_AsioAcceptor(m_AsioContext, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
+            {
+            }
+            virtual ~Server_Interface()
+            {
+            }
+
+            bool start_server()
+            {
+                try
+                {
+                    wait_for_client_connection();
+                    m_ContextThread = std::thread([this]()
+                                                  { m_AsioContext.run(); });
+                }
+                catch (const std::exception &e)
+                {
+                    // throw a net error here
+                    // std::cerr << e.what() << '\n';
+                    return false;
+                }
+
+                // inform that new connection was made
+                return true;
+            }
+
+            void stop_server()
+            {
+                m_AsioContext.stop();
+
+                if (m_ContextThread.joinable())
+                    m_ContextThread.join();
+
+                // inform about server stop
+            }
+
+            void wait_for_client_connection()
+            {
+                m_AsioAcceptor.async_accept(
+                    [this](std::error_code ec, asio::ip::tcp::socket socket)
+                    {
+                        if (!ec)
+                        {
+                            std::shared_ptr<Connection<T>> new_connection = std::make_shared<Connection<T>>(Connection<T>::owner::server, m_AsioContext, std::move(socket), m_qMessagesIn);
+
+                            if (on_client_connect(new_connection))
+                            {
+                                m_deqConnections.push_back(std::move(new_connection));
+
+                                m_deqConnections.back()->connect_to_client(m_nIDCounter++);
+                            }
+                            else
+                            {
+                                // inform that client has been denied
+                            }
+                        }
+                        else
+                        {
+                            // new connection error
+                        }
+
+                        wait_for_client_connection();
+                    });
+            }
+
+            void message_client(std::shared_ptr<Connection<T>> client, const Message<T> msg)
+            {
+                if(client && client->is_connected())
+                {
+                    client->send(msg);
+                }
+                else
+                {
+                    on_client_disconnect(client);
+                    client.reset();
+                    m_deqConnections.erase(std::remove(m_deqConnections.begin(), m_deqConnections.end(), client), m_deqConnections.end());
+                }
+            }
+
+            void message_all_clients(const Message<T>& msg, std::shared_ptr<Connection<T>> pIgnoreClient = nullptr)
+            {
+                bool invalid_client_exists = false;
+
+                for(auto &client : m_deqConnections)
+                {
+                    if(client && client->is_connected())
+                    {
+                        if(client != pIgnoreClient)
+                            client->send(msg);
+                    }
+                    else
+                    {
+                        on_client_disconnect(client);
+                        client.reset();
+                        invalid_client_exists = true;
+                    }
+                }
+
+                if(invalid_client_exists)
+                {
+                    m_deqConnections.erase(std::remove(m_deqConnections.begin(), m_deqConnections.end(), nullptr), m_deqConnections.end());
+                }
+            }
+
+            void update(size_t nMaxMessages = -1)
+            {
+                size_t nMessageCount = 0;
+                while(nMessageCount < nMaxMessages && !m_qMessagesIn.empty())
+                {
+                    auto msg = m_qMessagesIn.pop_front();
+                    on_message(msg.remote, msg.msg);
+                    nMessageCount++;
+                }
+            }
+
+        protected:
+            virtual bool on_client_connect(std::shared_ptr<Connection<T>> client)
+            {
+                return false;
+            }
+            virtual void on_client_disconnect(std::shared_ptr<Connection<T>> client)
+            {
+            }
+            virtual void on_message(std::shared_ptr<Connection<t>> client, Message<T> &msg)
+            {
+            }
+            
         private:
+            asio::io_context m_AsioContext;
+            std::thread m_ContextThread;
+            asio::ip::tcp::acceptor m_AsioAcceptor;
+
             TSQueue<OwnedMessage<T>> m_qMessagesIn;
 
-            std::deque<std::shared_ptr<Connection<T>> m_deqConnections;
-        }
+            std::deque < std::shared_ptr<Connection<T>> m_deqConnections;
+
+            uint32_t m_nIDCounter = 10000;
+        };
     }
 }
